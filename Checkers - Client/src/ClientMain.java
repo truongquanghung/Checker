61c6337a60768fed95ffa576b470d59a923fff95
truongquanghung <truongquanghunghn@gmail.com>
2020-10-21 08:05:05 +0700

    First upload
    Đã bao gồm nhảy nhiều bước

---
+++ Checkers - Client/src/ClientMain.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+import View.BoardPanel;
+
+/**
+ * Client Application -> Main
+ * @author Keerthikan
+ * 
+ * Main
+ */
+public class ClientMain {
+	
+	public static void main(String[] args) {
+		ClientApp client = new ClientApp();
+		client.setTitle("Checkers");
+		client.pack();
+		client.setVisible(true);
+		client.setLocation(250, 150);
+		client.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
